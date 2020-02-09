8ae477333fd0b9a046d65e68cea730cc4d278db0
are53 <are53@drexel.edu>
2020-02-09 14:13:25 -0500

    Start menu and module for the hints

--- hangman.py
+++ hangman.py
@@ -1,1 +1,22 @@
-print('hello world')
+import random #To give a random song from my lists
+import secret #Module that holds the hints for each song
+import sys
+
+hipHop = ['The Box']
+
+def gameLoop():
+    print('Hello')
+
+def startMenu():
+    option = str(input('Welcome to MusicMan\n- - - - - - - - - -\n  Start or Quit\n'))
+    option.lower()
+    if option == 'start':
+        print()
+        gameLoop()
+    elif option == 'quit':
+        sys.exit()
+    else:
+        print('\n\nOption not recognized. Try again.\n')
+        startMenu()
+
+startMenu()
---
+++ secret.py
@@ -0,0 +1,18 @@
+import random
+
+hiphop = {
+    'The Box':'The beginning of the song has the sound when you wipe a mirror',
+    'Life is Good' : 'OVO X FBGM',
+    'High Fashion' : 'Up style',
+    'Ballin\'' : 'The producer of this song is named after a condiment',
+    'BOP' : 'The jabbawockeez danced in the music video of this song',
+    'Woah' : 'A word you say when something amazes you',
+    'Dreams and Nightmares' : 'The philly anthem'
+}
+
+pop = {
+    'Yummy' : 'A word to describe your food',
+    'Don\'nt start now' : 'ABC',
+    'Falling' : 'This song became viral due to the app Tik Tok',
+    
+}
